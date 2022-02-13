937502e945daae9e7fa49fdfe3c07bdafbecc2a5
halilatilla <atillahalil0@gmail.com>
2022-02-13 18:09:20 +0300

    Add TodoItemMenu

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -13,7 +13,7 @@ const Button: FC<Props> = ({ label, onClick, className, children, disabled, type
   return (
     <button
       className={classnames(
-        'h-[42px] rounded-lg border border-gray-300 px-4 text-sm font-medium capitalize shadow-sm hover:bg-gray-700 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:select-none disabled:opacity-50 disabled:hover:bg-transparent',
+        'h-[42px] rounded-lg border border-gray-300 px-4 text-sm font-medium capitalize shadow-sm hover:bg-gray-600 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:select-none disabled:opacity-50 disabled:hover:bg-transparent',
         className,
       )}
       onClick={onClick}
